3ed2c653316d6ab3054038d92412787c88cb180a
Antonio S. Dromundo <sebastiandromundo@outlook.com>
2023-11-07 01:09:07 -0600

    works

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -73,8 +73,8 @@ class MyApp extends StatelessWidget {
             // Define a light and dark color theme. Then, read the user's
             // preferred ThemeMode (light, dark, or system default) from the
             // SettingsController to display the correct theme.
-            theme: ThemeManager.getLightTheme(light),
-            darkTheme: ThemeManager.getDarkTheme(dark),
+            theme: ThemeManager.getLightTheme(colorScheme:light),
+            darkTheme: ThemeManager.getDarkTheme(colorScheme: dark),
             themeMode: settingsController.themeMode,
             // Define a function to handle named routes in order to support
             // Flutter web url navigation and deep linking.
--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -3,23 +3,42 @@ import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 
+
 class ThemeManager {
-  static ColorScheme getDynamicColorScheme(ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-    if (lightDynamic != null && darkDynamic != null) {
-      // On Android S+ devices, use the provided dynamic color scheme.
-      // (Recommended) Harmonize the dynamic color scheme's built-in semantic colors.
-      return lightDynamic.harmonized();
-    } else {
-      // Otherwise, use fallback schemes.
-      return lightColorScheme;
-    }
+
+  static ThemeData getLightTheme({ColorScheme? colorScheme = lightColorScheme}) {
+    return ThemeData(
+      colorScheme: colorScheme,
+       useMaterial3: true);
   }
 
-  static ThemeData getLightTheme(ColorScheme colorScheme) {
-    return ThemeData(colorScheme: colorScheme, useMaterial3: true);
+  static ThemeData getDarkTheme({ColorScheme? colorScheme = darkColorScheme}) {
+    return ThemeData(
+      colorScheme: colorScheme,
+      brightness: Brightness.dark,
+      useMaterial3: true);
+  }
+}
+class ThemedApp extends StatelessWidget {
+
+  const ThemedApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+
+        return MaterialApp(
+            // Define a light and dark color theme. Then, read the user's
+            // preferred ThemeMode (light, dark, or system default) from the
+            // SettingsController to display the correct theme.
+            theme: ThemeManager.getLightTheme(colorScheme:lightDynamic),
+            darkTheme: ThemeManager.getDarkTheme(colorScheme:darkDynamic),
+
+        );
+      }
+    );
   }
 
-  static ThemeData getDarkTheme(ColorScheme colorScheme) {
-    return ThemeData(colorScheme: colorScheme, brightness: Brightness.dark, useMaterial3: true);
-  }
 }
